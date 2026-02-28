a0993dd0950fac87db32b212b472a398645a304a
Bulat Zaripov <bulat_zaripov@bk.ru>
2026-02-28 11:58:41 +0300

    Initial commit

---
+++ lesson3.py
@@ -0,0 +1,50 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+LOGIN=os.environ['LOGIN']
+TOKEN=os.environ['TOKEN']
+
+text="""\
+Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+%website% — это новая версия онлайн-курса по программированию. 
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
+
+Как будет проходить ваше обучение на %website%? 
+
+→ Попрактикуешься на реальных кейсах. 
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей. 
+Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
+
+Регистрируйся → %website%  
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
+website="https://dvmn.org/profession-ref-program/bulat_zaripov/jdqAd/"
+friend_name="Никита"
+my_name="Булат Зарипов"
+
+text=text.replace("%website%", website)
+text=text.replace("%friend_name%", friend_name)
+text=text.replace("%my_name%", my_name)
+
+title="""\
+From: {0}
+To: {1}
+Subject: Курсы Devman
+Content-Type: text/plain; charset="UTF-8";\n
+"""
+friend_mail="Bulat789145@yandex.ru"
+
+title=title.format(LOGIN, friend_mail)
+letter=title+text
+
+letter=letter.encode("UTF-8")
+server=smtplib.SMTP_SSL('smtp.mail.ru', 465)
+server.login(LOGIN, TOKEN)
+server.sendmail(LOGIN, friend_mail, letter)
+server.quit()
+
